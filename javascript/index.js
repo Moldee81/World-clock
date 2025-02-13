b2191fccb9890237fbc20352ba90cac8680caaf9
Moldee81 <a.penatomczak@gmail.com>
2025-02-13 23:17:13 +0000

    JS updated

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,6 @@
 function updateTime() {
   let poznanElement = document.querySelector("#poznan");
   let poznanDateElement = poznanElement.querySelector(".date");
-  poznanDateElement.innerHTML = moment()
-    .tz("Europe/Warsaw")
-    .format("MMMM Do YYYY");
   let poznanTimeElement = poznanElement.querySelector(".time");
   poznanTimeElement.innerHTML = moment()
     .tz("Europe/Warsaw")
@@ -11,9 +8,6 @@ function updateTime() {
 
   let sydneyElement = document.querySelector("#sydney");
   let sydneyDateElement = sydneyElement.querySelector(".date");
-  sydneyDateElement.innerHTML = moment()
-    .tz("Australia/Sydney")
-    .format("MMMM Do YYYY");
   let sydneyTimeElement = sydneyElement.querySelector(".time");
   sydneyTimeElement.innerHTML = moment()
     .tz("Australia/Sydney")
