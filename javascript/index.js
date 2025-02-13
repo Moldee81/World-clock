1da03e235b57998f4de5b5c7452b897baa01b002
Moldee81 <a.penatomczak@gmail.com>
2025-02-13 13:01:13 +0000

    Added JS time and date function to the clock with moment.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let poznanElement = document.querySelector("#poznan");
+  let poznanDateElement = poznanElement.querySelector(".date");
+  poznanDateElement.innerHTML = moment()
+    .tz("Europe/Warsaw")
+    .format("MMMM Do YYYY");
+  let poznanTimeElement = poznanElement.querySelector(".time");
+  poznanTimeElement.innerHTML = moment()
+    .tz("Europe/Warsaw")
+    .format("h:mm:ss [<small>]A[</small>]");
 
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  sydneyDateElement.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("MMMM Do YYYY");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  sydneyTimeElement.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("h:mm:ss[<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
